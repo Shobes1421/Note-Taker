03e8b35a9d8528880f80465ac1d96b9b1eb71008
David Shober <shobes1421@hotmail.com>
2020-07-24 13:10:01 -0600

    updated all code for app to work

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+// add required modules
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const path = require("path");
 
-// Initialize the app and create a port
+// Create server application at port 3000
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set up body parsing, static, and route middleware
+// Read URL or JSON
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
 
-// Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+// Include js files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Use public folder
+app.use(express.static("public"));
+
+// Add listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
